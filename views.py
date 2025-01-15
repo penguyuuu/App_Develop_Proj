09ffc9dc55d44f8d7c274cee18149c2cf4b3f1db
jiaen1234 <engjiaen10@gmail.com>
2025-01-15 14:43:37 +0800

    Update views.py

--- views.py
+++ views.py
@@ -1,3 +1,4 @@
+from flask import Flask, render_template
 from flask import Blueprint, render_template
 
 views = Blueprint('views',__name__)
@@ -5,4 +6,3 @@ views = Blueprint('views',__name__)
 @views.route('/')
 def home():
     return render_template('base.html')
-
